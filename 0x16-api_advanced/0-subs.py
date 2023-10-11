52988e670a1e905f776eeb96543e7eeb5511e1cb
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-10-11 01:23:37 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API and returns the number of subscribers for a given
+    subreddit. If an invalid subreddit is given, the function should return 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        "User-Agent": "Custom User Agent"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        else:
+            return 0  # Return 0 for invalid subreddits or other errors
+    except Exception as e:
+        return 0

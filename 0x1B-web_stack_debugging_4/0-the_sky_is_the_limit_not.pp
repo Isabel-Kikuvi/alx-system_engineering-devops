4c74f6edcf2603651da977ca673124fa7e590393
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-10-19 10:56:02 -0700

    puppet lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Manifest that increases traffic an Nginx server can handle
 
 exec { 'fix--for-nginx':
-  command => "sed -i "s/15/4096/" /etc/default/nginx",
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
 
